c06ebc7086293d1a59fcd6d6b31ea32a635ee599
Kaewmanee Marasri <kaewmanee.m@cdg.co.th>
2025-05-14 16:22:47 +0700

    feat: update project structure and dependencies, add Vuetify integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/eslint']
+  
+ build: {
+    transpile: ['vuetify'],
+  },
+modules: ['@nuxt/fonts', '@nuxt/eslint', (_options, nuxt) => {
+    nuxt.hooks.hook('vite:extendConfig', (config) => {
+      // @ts-expect-error
+      config.plugins.push(vuetify({ autoImport: true }))
+    })
+  },],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  srcDir: './src',
 })
